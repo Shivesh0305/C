356f49249dd26a856f57c6d6169b32eb0885252a
Shivesh Upadhyay <162001314+Shivesh0305@users.noreply.github.com>
2025-04-14 08:41:51 +0530

    Delete Chapter-8 directory

--- Chapter-8/strings.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    // char str[]={'S','H','I','V','E','S','H','\0'};
-    char str[]="SHIVESH";
-    char *ptr=&str[0];
-    printf("%u",ptr);
-    return 0;
-}
--- Chapter-8/strlen.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-
-int main(){
-    char *s="shivesh";
-    int a=strlen(s);
-    printf("%d",a);
-    return 0;
-}
